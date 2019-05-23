e059c1cf3e442a293409df5fad6c964a5d91cf88
JHR <jianghairui@sina.cn>
2019-05-23 17:38:40 +0800

    联升房地产小程序完成

--- application/estate/controller/api/Api.php
+++ application/estate/controller/api/Api.php
@@ -75,6 +75,7 @@ class Api extends Common {
             $where[] = ['del','=',0];
             $list = Db::table('mp_resource')
                 ->where($where)
+                ->field("id,pic,name,avr_price,min_bro,max_bro,tags")
                 ->limit(($curr_page-1)*$perpage,$perpage)
                 ->order($order)
                 ->select();
@@ -82,6 +83,23 @@ class Api extends Common {
             return ajax($e->getMessage(), -1);
         }
         return ajax($list);
+    }
+
+    public function resourceDetail() {
+        $val['id'] = input('post.id');
+        checkPost($val);
+        try {
+            $where = [
+                ['id','=',$val['id']]
+            ];
+            $exist = Db::table('mp_resource')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法参数',-4);
+            }
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax($exist);
     }
 
     public function apartmentList() {
@@ -104,5 +122,110 @@ class Api extends Common {
         return ajax($list);
     }
 
+    public function apartmentDetail() {
+        $val['id'] = input('post.id');
+        checkPost($val);
+        try {
+            $where = [
+                ['a.id','=',$val['id']]
+            ];
+            $exist = Db::table('mp_apartment')->alias('a')
+                ->join("mp_resource r","a.res_id=r.id","left")
+                ->where($where)
+                ->field("a.*,r.name")
+                ->find();
+            if(!$exist) {
+                return ajax('非法参数',-4);
+            }
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        $exist['pic'] = unserialize($exist['pic']);
+        return ajax($exist);
+    }
+
+    public function appointment() {
+        $val['res_id'] = input('post.res_id');
+        $val['name'] = input('post.name');
+        $val['tel'] = input('post.tel');
+        $val['meeting_date'] = input('post.meeting_date');
+        $val['uid'] = $this->myinfo['uid'];
+        checkPost($val);
+        $val['create_time'] = time();
+        if(!is_tel($val['tel'])) {
+            return ajax('无效的手机号',6);
+        }
+        if(!is_date($val['meeting_date'])) {
+            return ajax('无效的日期',48);
+        }
+        try {
+            $whereRes = [
+                ['id','=',$val['res_id']]
+            ];
+            $res_exist = Db::table('mp_resource')->where($whereRes)->find();
+            if(!$res_exist) {
+                return ajax('非法参数',-4);
+            }
+            $whereAppoint = [
+                ['res_id','=',$val['res_id']],
+                ['uid','=',$this->myinfo['uid']]
+            ];
+            $appoint_exist = Db::table('mp_appoint')->where($whereAppoint)->find();
+            if($appoint_exist) {
+                return ajax('已预约',47);
+            }
+            Db::table('mp_appoint')->insert($val);
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
+    }
+
+    public function aboutUs() {
+        try {
+            $info = Db::table('mp_company')->find();
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax($info);
+    }
+
+    public function appointList() {
+        $curr_page = input('post.page',1);
+        $perpage = input('post.perpage',10);
+        try {
+            $where = [
+                ['uid','=',$this->myinfo['uid']]
+            ];
+            $list = Db::table('mp_appoint')
+                ->where($where)
+                ->limit(($curr_page-1)*$perpage,$perpage)->select();
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax($list);
+    }
+
+    public function appointCancel() {
+        $val['id'] = input('post.id');
+        checkPost($val);
+        try {
+            $where = [
+                ['id','=',$val['id']],
+                ['uid','=',$this->myinfo['uid']]
+            ];
+            $exist = Db::table('mp_appoint')->where($where)->find();
+            if(!$exist) {
+                return ajax('非法参数',-4);
+            }
+            if($exist['status'] == 1) {
+                return ajax('无法取消,请刷新重试',49);
+            }
+            Db::table('mp_appoint')->where($where)->delete();
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+        return ajax();
+    }
 
 }
