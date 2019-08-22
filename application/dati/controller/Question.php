a64cc06782aedfcac7327ac4aad3880f7d033874
Jiang <1873645345@qq.com>
2019-08-22 18:01:02 +0800

    答题小程序后台完成

--- application/dati/controller/Question.php
+++ application/dati/controller/Question.php
@@ -16,19 +16,17 @@ class Question extends Base {
         $curr_page = input('param.page',1);
         $perpage = input('param.perpage',10);
         $where = [];
-        if($param['search']) {
-            $where[] = ['a.title|a.desc','like',"%{$param['search']}%"];
-        }
-        $count = Db::table('mp_question')->alias('a')->where($where)->count();
+        $count = Db::table('mp_chapter')->alias('c')->where($where)->count();
 
         $page['count'] = $count;
         $page['curr'] = $curr_page;
         $page['totalPage'] = ceil($count/$perpage);
         try {
-            $list = Db::table('mp_question')->alias('a')
-                ->join('mp_admin ad','a.admin_id=ad.id','left')
-                ->field('a.*,ad.realname')
-                ->where($where)->limit(($curr_page - 1)*$perpage,$perpage)->select();
+            $list = Db::table('mp_chapter')->alias('c')
+                ->join('mp_admin ad','c.admin_id=ad.id','left')
+                ->field('c.*,ad.realname')
+                ->where($where)
+                ->select();
         }catch (\Exception $e) {
             die('SQL错误: ' . $e->getMessage());
         }
@@ -41,22 +39,10 @@ class Question extends Base {
     public function chapterAdd() {
         if(request()->isPost()) {
             $val['title'] = input('post.title');
-            $val['desc'] = input('post.desc');
-            $val['status'] = input('post.status');
             checkInput($val);
-            $val['content'] = input('post.content');
             $val['admin_id'] = session('admin_id');
-
-            if(isset($_FILES['file'])) {
-                $info = upload(array_keys($_FILES)[0],$this->upload_base_path);
-                if($info['error'] === 0) {
-                    $val['pic'] = $info['data'];
-                }else {
-                    return ajax($info['msg'],-1);
-                }
-            }
             try {
-                Db::table('mp_question')->insert($val);
+                Db::table('mp_chapter')->insert($val);
             }catch (\Exception $e) {
                 if(isset($val['pic'])) {
                     @unlink($val['pic']);
@@ -71,7 +57,7 @@ class Question extends Base {
     public function chapterDetail() {
         $question_id = input('param.id');
         try {
-            $exist = Db::table('mp_question')->where('id',$question_id)->find();
+            $exist = Db::table('mp_chapter')->where('id',$question_id)->find();
             if(!$exist) {
                 $this->error('非法操作');
             }
@@ -84,32 +70,13 @@ class Question extends Base {
 
     public function chapterMod() {
         $val['title'] = input('post.title');
-        $val['desc'] = input('post.desc');
-        $val['status'] = input('post.status');
         $val['id'] = input('post.id');
         checkInput($val);
-        $val['content'] = input('post.content');
         $val['admin_id'] = session('admin_id');
-
-        if(isset($_FILES['file'])) {
-            $info = upload('file',$this->upload_base_path);
-            if($info['error'] === 0) {
-                $val['pic'] = $info['data'];
-            }else {
-                return ajax($info['msg'],-1);
-            }
-        }
         try {
-            $exist = Db::table('mp_question')->where('id',$val['id'])->find();
-            Db::table('mp_question')->update($val);
+            Db::table('mp_chapter')->update($val);
         }catch (\Exception $e) {
-            if(isset($val['pic'])) {
-                @unlink($val['pic']);
-            }
             return ajax($e->getMessage(),-1);
-        }
-        if(isset($val['pic'])) {
-            @unlink($exist['pic']);
         }
         return ajax();
     }
@@ -134,64 +101,88 @@ class Question extends Base {
     //题目列表
     public function questionList()
     {
-        $param['search'] = input('param.search');
+        $param['search'] = input('param.search','');
+        $param['c_id'] = input('param.c_id','');
         $page['query'] = http_build_query(input('param.'));
 
         $curr_page = input('param.page',1);
         $perpage = input('param.perpage',10);
         $where = [];
         if($param['search']) {
-            $where[] = ['a.title|a.desc','like',"%{$param['search']}%"];
+            $where[] = ['title','like',"%{$param['search']}%"];
         }
-        $count = Db::table('mp_question')->alias('a')->where($where)->count();
+        if($param['c_id']) {
+            $where[] = ['c_id','=',$param['c_id']];
+        }
+        $count = Db::table('mp_question')->where($where)->count();
 
         $page['count'] = $count;
         $page['curr'] = $curr_page;
         $page['totalPage'] = ceil($count/$perpage);
         try {
-            $list = Db::table('mp_question')->alias('a')
-                ->join('mp_admin ad','a.admin_id=ad.id','left')
-                ->field('a.*,ad.realname')
+            $list = Db::table('mp_question')
                 ->where($where)->limit(($curr_page - 1)*$perpage,$perpage)->select();
+            $chapterlist = Db::table('mp_chapter')->select();
         }catch (\Exception $e) {
             die('SQL错误: ' . $e->getMessage());
         }
 
         $this->assign('list',$list);
+        $this->assign('chapterlist',$chapterlist);
         $this->assign('page',$page);
+        $this->assign('param',$param);
         return $this->fetch();
     }
     //添加题目页面
     public function questionAdd() {
-        return $this->fetch();
-    }
-    //添加题目提交
-    public function questionAddPost() {
-        $val['title'] = input('post.title');
-        $val['desc'] = input('post.desc');
-        $val['status'] = input('post.status');
-        checkInput($val);
-        $val['content'] = input('post.content');
-        $val['admin_id'] = session('admin_id');
-
-        if(isset($_FILES['file'])) {
-            $info = upload(array_keys($_FILES)[0],$this->upload_base_path);
-            if($info['error'] === 0) {
-                $val['pic'] = $info['data'];
-            }else {
-                return ajax($info['msg'],-1);
+        if(request()->isPost()) {
+            $val['title'] = input('post.title');
+            $val['c_id'] = input('post.c_id');
+            $val['num'] = input('post.num');
+            $val['key'] = input('post.key');
+            switch ($val['num']) {
+                case '2':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    ;break;
+                case '3':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    ;break;
+                case '4':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    $val['option_d'] = input('post.option_d');
+                    ;break;
+                case '5':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    $val['option_d'] = input('post.option_d');
+                    $val['option_e'] = input('post.option_e');
+                    ;break;
             }
+            checkInput($val);
+            $val['excerpt'] = input('post.excerpt');
+            $val['admin_id'] = session('admin_id');
+            try {
+                Db::table('mp_question')->insert($val);
+            }catch (\Exception $e) {
+                return ajax($e->getMessage(),-1);
+            }
+            return ajax([]);
         }
         try {
-            Db::table('mp_question')->insert($val);
-        }catch (\Exception $e) {
-            if(isset($val['pic'])) {
-                @unlink($val['pic']);
-            }
-            return ajax($e->getMessage(),-1);
+            $list = Db::table('mp_chapter')->select();
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
         }
-        return ajax([]);
-
+        $param['c_id'] = input('param.c_id','');
+        $this->assign('list',$list);
+        $this->assign('param',$param);
+        return $this->fetch();
     }
     //修改题目页面
     public function questionDetail() {
@@ -201,44 +192,56 @@ class Question extends Base {
             if(!$exist) {
                 $this->error('非法操作');
             }
+            $list = Db::table('mp_chapter')->select();
         } catch (\Exception $e) {
             die($e->getMessage());
         }
         $this->assign('info',$exist);
+        $this->assign('list',$list);
         return $this->fetch();
     }
     //修改题目提交
-    public function questionModPost() {
-        $val['title'] = input('post.title');
-        $val['desc'] = input('post.desc');
-        $val['status'] = input('post.status');
-        $val['id'] = input('post.id');
-        checkInput($val);
-        $val['content'] = input('post.content');
-        $val['admin_id'] = session('admin_id');
-
-        if(isset($_FILES['file'])) {
-            $info = upload('file',$this->upload_base_path);
-            if($info['error'] === 0) {
-                $val['pic'] = $info['data'];
-            }else {
-                return ajax($info['msg'],-1);
+    public function questionMod() {
+        if(request()->isPost()) {
+            $val['title'] = input('post.title');
+            $val['c_id'] = input('post.c_id');
+            $val['num'] = input('post.num');
+            $val['key'] = input('post.key');
+            $val['id'] = input('post.id');
+            switch ($val['num']) {
+                case '2':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    ;break;
+                case '3':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    ;break;
+                case '4':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    $val['option_d'] = input('post.option_d');
+                    ;break;
+                case '5':
+                    $val['option_a'] = input('post.option_a');
+                    $val['option_b'] = input('post.option_b');
+                    $val['option_c'] = input('post.option_c');
+                    $val['option_d'] = input('post.option_d');
+                    $val['option_e'] = input('post.option_e');
+                    ;break;
             }
-        }
-        try {
-            $exist = Db::table('mp_question')->where('id',$val['id'])->find();
-            Db::table('mp_question')->update($val);
-        }catch (\Exception $e) {
-            if(isset($val['pic'])) {
-                @unlink($val['pic']);
+            checkInput($val);
+            $val['excerpt'] = input('post.excerpt');
+            $val['admin_id'] = session('admin_id');
+            try {
+                Db::table('mp_question')->update($val);
+            }catch (\Exception $e) {
+                return ajax($e->getMessage(),-1);
             }
-            return ajax($e->getMessage(),-1);
+            return ajax([]);
         }
-        if(isset($val['pic'])) {
-                @unlink($exist['pic']);
-        }
-        return ajax();
-
     }
     //删除题目
     public function questionDel() {
@@ -256,38 +259,6 @@ class Question extends Base {
         @unlink($exist['pic']);
         return ajax([],1);
     }
-    //停用题目
-    public function questionHide()
-    {
-        $val['id'] = input('post.id');
-        checkInput($val);
-        try {
-            $exist = Db::table('mp_question')->where('id', $val['id'])->find();
-            if (!$exist) {
-                return ajax('非法操作', -1);
-            }
-            Db::table('mp_question')->where('id', $val['id'])->update(['status' => 0]);
-        } catch (\Exception $e) {
-            return ajax($e->getMessage(), -1);
-        }
-        return ajax();
-    }
-    //启用题目
-    public function questionShow() {
-        $val['id'] = input('post.id');
-        checkInput($val);
-        try {
-            $exist = Db::table('mp_question')->where('id',$val['id'])->find();
-            if(!$exist) {
-                return ajax('非法操作',-1);
-            }
-            Db::table('mp_question')->where('id',$val['id'])->update(['status'=>1]);
-        } catch (\Exception $e) {
-            return ajax($e->getMessage(), -1);
-        }
-        return ajax();
-    }
-
 
 
 
